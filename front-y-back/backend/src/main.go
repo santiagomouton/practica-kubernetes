fb70a4332399a0f737d8b668cbe3291e5cc7d96f
santiagomouton <santiago.mouton@mi.unc.edu.ar>
2021-12-25 20:16:40 -0300

    cliente servidor

---
+++ front-y-back/backend/src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Response struct {
+	Time     time.Time `json:"time"`
+	Hostname string    `json:"hostname"`
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	resp := Response{
+		Time:     time.Now(),
+		Hostname: os.Getenv("HOSTNAME"),
+	}
+
+	jsonResp, err := json.Marshal(&resp)
+	if err != nil {
+		w.Write([]byte("error"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)
+}
+
+func main() {
+	http.HandleFunc("/", ServeHTTP)
+	http.ListenAndServe(":9090", nil)
+}
